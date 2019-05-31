776216d850d933bfc61f04ac3073f27fe8995832
nantakah <nantakah@yahoo-corp.jp>
2019-05-31 18:00:22 +0900

    SP用のHTML,CSS追加しました

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,16 @@
-$('.mainvisual__images').slick({
-  infinite: true,
-  speed: 500,
-  fade: true,
-  cssEase: 'linear',
-  autoplay: true
+$(document).ready(function(){
+  $('.menubtn').on('click', function(){
+    $('#globalnav').slideToggle();
+    $('html, body').toggleClass('hidden');
+    return false;
+  });
+
+  $('.mainvisual__images').slick({
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true
+  });
 });
+
